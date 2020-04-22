f03ce2ea2d9eaadff785fa99e058a10316b7651c
Madsoffee Yako <s5935512073@phuket.psu.ac.th>
2020-04-22 18:38:56 +0700

    facebook login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Topbar from './component/Topbar.js';
 
-function App() {
+const App = () => {
   return(
     <div>
       <Topbar/>
--- src/Auth.js
+++ src/Auth.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Facebook from './component/Facebook';
+import './Auth.css';
 
 const Auth = () => {
+
+    const [name,setName] = useState('');
+    const response = () => {
+        setName()
+    }
+    console.log(response.name)
     return(
-        <div>
-            Auth
+        <div className="auth">
+            <Facebook response={response} />
+            <h1>111 {name}</h1>
         </div>
+        
     )
+    console.log(response.name)
 }
 
 export default Auth;
